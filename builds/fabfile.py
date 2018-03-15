ef1177ce5c78164b6d86bd2d99eb116a926d47f5
codylane <cody.lane@gmail.com>
2018-03-15 09:11:50 -0600

    updates for gitea-1.3.3

--- builds/fabfile.py
+++ builds/fabfile.py
@@ -20,7 +20,7 @@ GITEA_HTTP_PORT = GITEA_PORT
 
 DEFAULT_SPEC_DATA = {
     'package_name': 'gitea',
-    'package_version': '1.3',
+    'package_version': '1.3.3',
     'package_release': '1',
 
     'distro': 'linux',
